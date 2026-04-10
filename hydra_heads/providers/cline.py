f381158c9f91879a86d85f6286c69ef9d9e0c422
CJ Angrist <cjangrist@gmail.com>
2026-04-10 07:55:54 -0400

    Change model_detect_command to use tail instead of head

--- hydra_heads/providers/cline.py
+++ hydra_heads/providers/cline.py
@@ -5,6 +5,6 @@ PROVIDER = {
     "binary": "cline",
     "args": ["-y"],
     "prompt_flag": None,
-    "model_detect_command": """grep -oi 'modelId": "[^"]*"' ~/.cline/data/globalState.json 2>/dev/null | head -1 | cut -d'"' -f3 | awk -F/ '{print $NF}'""",
+    "model_detect_command": """grep -oi 'modelId": "[^"]*"' ~/.cline/data/globalState.json 2>/dev/null | tail -1 | cut -d'"' -f3 | awk -F/ '{print $NF}'""",
     "env": {},
 }
